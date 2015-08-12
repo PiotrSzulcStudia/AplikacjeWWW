5631b8316c42eb01894e656fc772effdc2b92e4b
Piotr Szulc <kkragoth@gmail.com>
2015-08-12 18:42:17 +0200

    Index basic test

--- wsgi/myproject/vote2/views.py
+++ wsgi/myproject/vote2/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import District, Commission
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world")
+    voievodships = District.objects.filter(parentId=None)
+    context = {'voievodships': voievodships}
+    return render(request, 'vote2/index.html', context)
